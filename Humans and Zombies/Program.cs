7e880c2325496afa88e8be6f301d85d1c1c11954
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-05-27 19:22:16 +0100

    Add project files.

---
+++ Humans and Zombies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Humans_and_Zombies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
